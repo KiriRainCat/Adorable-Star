e741e35cc08cffc40eabdbfe16abbaf0eb60d0de
KiriRainCat <2287990465@qq.com>
2023-11-12 15:45:48 +0800

    🐳 chore: Prettier printWidth手动指定为120

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -2,7 +2,7 @@
   <div>
     <p>{{ title }}</p>
     <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
+      <li class="todoList" v-for="todo in todos" :key="todo.id" @click="increment">
         {{ todo.id }} - {{ todo.content }}
       </li>
     </ul>
