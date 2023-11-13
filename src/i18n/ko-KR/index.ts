db34f22f909621482d37755cea89df726aadf2b1
KiriRainCat <2287990465@qq.com>
2023-11-13 08:35:02 +0800

    ✨ feat: 国际化初始化与配置

---
+++ src/i18n/ko-KR/index.ts
@@ -0,0 +1,4 @@
+export default {
+  pageNotFound: '이런~ 길을 잃었나봐요...',
+  backToHome: '집으로 데려가주세요!',
+};
---
+++ src/i18n/zh-CN/index.ts
@@ -0,0 +1,4 @@
+export default {
+  pageNotFound: '哎呀~ 你好像迷路了呢...',
+  backToHome: '带我回家！',
+};
