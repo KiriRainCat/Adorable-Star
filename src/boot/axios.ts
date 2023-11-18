acc3ed12ad7a078ac1248b76ac03ec300afa773e
KiriRainCat <2287990465@qq.com>
2023-11-18 10:04:13 +0800

    ✨ feat: Axios拦截器配置

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -1,8 +1,44 @@
 import axios from 'axios';
+import { Notify } from 'quasar';
 
 // Create global axios instance
 const api = axios.create({
   baseURL: '/api',
+  headers: {
+    Authorization: 'dvgZgggolxESd0m',
+  },
 });
+
+// Add token to request headers
+api.interceptors.request.use((req) => {
+  req.headers['Token'] = localStorage.getItem('token');
+  return req;
+});
+
+// Handle common response error and store incoming new token
+api.interceptors.response.use(
+  (res) => {
+    const token = res.headers['New-Token'];
+    if (token) {
+      localStorage.setItem('token', token);
+    }
+    return res;
+  },
+  (e) => {
+    switch (e.response.data.code) {
+      case 401:
+        localStorage.setItem('token', '');
+        break;
+      case 400:
+      case 500:
+        Notify.create({
+          type: 'negative',
+          message: e.response.data.msg,
+        });
+        break;
+    }
+    return Promise.reject(e);
+  },
+);
 
 export { api };
