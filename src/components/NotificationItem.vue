fa488502d17ed8c0a67b1491c3f29065fca32966
KiriRainCat <2287990465@qq.com>
2023-11-20 06:47:49 +0800

    🐞 fix: 删除通知的按钮分离到item外，不然会触发两个点击事件

--- src/components/NotificationItem.vue
+++ src/components/NotificationItem.vue
@@ -1,49 +1,54 @@
 <template>
-  <q-item clickable class="flex" :to="assignment === undefined ? `/data/course/${from}` : `/data/assignment/${from}`">
-    <span v-if="type == undefined" class="w-1 h-8 bg-pink-300 mr-2"></span>
-    <span v-if="type == 1" class="w-1 h-8 bg-cyan-300 mr-2"></span>
-    <span v-if="type == 2" class="w-1 h-8 bg-indigo-300 mr-2"></span>
+  <div class="flex">
+    <q-item
+      clickable
+      class="flex flex-1"
+      :to="assignment === undefined ? `/data/course/${from}` : `/data/assignment/${from}`"
+    >
+      <span v-if="type == undefined" class="w-1 h-8 bg-pink-300 mr-2"></span>
+      <span v-if="type == 1" class="w-1 h-8 bg-cyan-300 mr-2"></span>
+      <span v-if="type == 2" class="w-1 h-8 bg-indigo-300 mr-2"></span>
 
-    <span class="flex-1">
-      <q-item-section>
-        <q-item-label>{{ course }}</q-item-label>
-        <q-item-label caption>{{ formatTime(created_at) }}</q-item-label>
-      </q-item-section>
-    </span>
+      <span class="flex-1">
+        <q-item-section>
+          <q-item-label>{{ course }}</q-item-label>
+          <q-item-label caption>{{ formatTime(created_at) }}</q-item-label>
+        </q-item-section>
+      </span>
 
-    <span class="flex-1">
-      <q-item-section v-if="type == undefined">
-        <q-tooltip v-if="assignment!.length > 37">{{ msg.split('|')[1] }}</q-tooltip>
+      <span class="flex-1">
+        <q-item-section v-if="type == undefined">
+          <q-tooltip v-if="assignment!.length > 37">{{ msg.split('|')[1] }}</q-tooltip>
 
-        <q-item-label>
-          {{ assignment!.length > 37 ? `${assignment!.substring(0, 39)}...` : assignment }}
-        </q-item-label>
-        <q-item-label caption>Due: {{ msg }}</q-item-label>
-      </q-item-section>
+          <q-item-label>
+            {{ assignment!.length > 37 ? `${assignment!.substring(0, 39)}...` : assignment }}
+          </q-item-label>
+          <q-item-label caption>Due: {{ msg }}</q-item-label>
+        </q-item-section>
 
-      <q-item-section v-if="type == 1">
-        <q-item-label v-if="msg.split('|')[0] === 'Desc'">
-          {{ $t('assignment') + $t('directions') + $t('changed') }}
-        </q-item-label>
-        <q-item-label v-else>
-          {{ $t(msg.split('|')[0].toLowerCase()) }} {{ $t('changed') }}: [{{ msg.split('|')[1] }} →
-          {{ msg.split('|')[2] }}]
-        </q-item-label>
-        <q-item-label caption>{{ $t('target') }}: {{ assignment }}</q-item-label>
-      </q-item-section>
+        <q-item-section v-if="type == 1">
+          <q-item-label v-if="msg.split('|')[0] === 'Desc'">
+            {{ $t('assignment') + $t('directions') + $t('changed') }}
+          </q-item-label>
+          <q-item-label v-else>
+            {{ $t(msg.split('|')[0].toLowerCase()) }} {{ $t('changed') }}: [{{ msg.split('|')[1] }} →
+            {{ msg.split('|')[2] }}]
+          </q-item-label>
+          <q-item-label caption>{{ $t('target') }}: {{ assignment }}</q-item-label>
+        </q-item-section>
 
-      <q-item-section v-if="type == 2">
-        <q-item-label>{{ msg.split('|')[1] }}</q-item-label>
-        <q-item-label caption>
-          {{ $t('previousGrade') }}: {{ msg.split('|')[0] === ' ' ? 'None' : msg.split('|')[0] }}
-        </q-item-label>
-      </q-item-section>
-    </span>
-
-    <q-btn flat round @click="onDeleteMsg">
+        <q-item-section v-if="type == 2">
+          <q-item-label>{{ msg.split('|')[1] }}</q-item-label>
+          <q-item-label caption>
+            {{ $t('previousGrade') }}: {{ msg.split('|')[0] === ' ' ? 'None' : msg.split('|')[0] }}
+          </q-item-label>
+        </q-item-section>
+      </span>
+    </q-item>
+    <q-btn flat @click="onDeleteMsg">
       <q-icon name="close" color="red-10" />
     </q-btn>
-  </q-item>
+  </div>
 </template>
 
 <script setup lang="ts">
