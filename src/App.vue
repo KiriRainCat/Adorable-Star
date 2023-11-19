a47ae145c24e08be03383b52dc94d73069128124
KiriRainCat <2287990465@qq.com>
2023-11-19 08:14:57 +0800

    ✨ feat: 添加通知信息本地存储

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,28 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { useAppStore } from './stores/app';
 
 const i18n = useI18n();
+const store = useAppStore();
 
 onMounted(() => {
+  // Locale
   i18n.locale.value = localStorage.getItem('locale')!;
+
+  // Get previous data from local storage
+  store.gpa = localStorage.getItem('gpa') || '';
+  store.fetchedAt = localStorage.getItem('fetchedAt') || '';
+  store.notifications = JSON.parse(localStorage.getItem('notifications') || '[]');
+
+  // Store data to local storage on page close
+  onbeforeunload = () => {
+    localStorage.setItem('gpa', store.gpa);
+    localStorage.setItem('fetchedAt', store.fetchedAt);
+    localStorage.setItem('notifications', JSON.stringify(store.notifications));
+  };
+
+  // Start long polling for notifications
 });
 </script>
 
